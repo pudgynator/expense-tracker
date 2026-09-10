1208fbb4aa5fe082778fe734a29fa7509306a388
Maria <dobrydnyk.m@gmail.com>
2026-09-10 11:40:57 +0300

    Update structure and styles

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,23 @@
+import { Mail, Lock, LogIn } from "lucide-react";
+import { AuthInput } from "../components/AuthInput";
+import { useState } from "react";
+
 
 export function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log({name, password});
+    }
+
     return (
         <div className="flex items-center justify-center bg-stone-100 px-4 min-h-screen ">
             <div className="flex flex-col min-w-sm py-8 px-4 items-center justify-center bg-white rounded-sm shadow-md">
                 <div className="flex flex-col gap-2 items-center justify-center mb-10">
-                    <img src="/images/login.png" alt="login icon" className="w-8 h-8"/>
+                    <LogIn className="h-6 w-6 text-lime-500" />
                     <p className="text-stone-600 text-xl font-bold ">
                         Welcome!
                     </p>
@@ -13,39 +26,19 @@ export function Login() {
                     </p>
                 </div>
 
-                <form className="flex flex-col gap-2 w-full px-4">
-                    <div className="relative mb-6">
-                        <label className="text-semibold mb-1 block text-sm text-stone-400">Name</label>
-                        <input
-                            type="text"
-                            className="w-full border-b border-stone-400 bg-transparent pb-2 pr-8 text-sm text-stone-100 outline-none focus:border-lime-400"
-                        />
-                        {/* <UserIcon className="absolute right-0 bottom-2 h-4 w-4 text-stone-500" /> */}
-                    </div>
-                    <div className="relative mb-6">
-                        <label className="text-semibold mb-1 block text-sm text-stone-400">E-mail</label>
-                        <input
-                            type="text"
-                            className="w-full border-b border-stone-400 bg-transparent pb-2 pr-8 text-sm text-stone-100 outline-none focus:border-lime-400"
-                        />
-                        {/* <UserIcon className="absolute right-0 bottom-2 h-4 w-4 text-stone-500" /> */}
-                    </div>
-                    <div className="relative mb-2">
-                        <label className="text-semibold mb-1 block text-sm text-stone-400">Password</label>
-                        <input
-                            type="text"
-                            className="w-full border-b border-stone-400 bg-transparent pb-2 pr-8 text-sm text-stone-100 outline-none focus:border-lime-400"
-                        />
-                        {/* <UserIcon className="absolute right-0 bottom-2 h-4 w-4 text-stone-500" /> */}
-                    </div>
+                <form className="flex flex-col gap-2 w-full px-4" onSubmit={handleSubmit}>
+                    <AuthInput label="E-mail" type="email"  icon={Mail} value={email} onChange={setEmail} />
+                    <AuthInput label="Password" type="password" icon={Lock} value={password} onChange={setPassword} />
 
-                    <div className="flex justify-between items-center mb-6">
-                        <div className="text-sm leading-none text-zinc-400">
-                            remember me?
-                        </div>
-                        <div className="text-sm leading-none">
-                            forgot password?
-                        </div>
+                    <div className="flex items-center justify-between mb-6">
+                        <label className="flex items-center leading-none gap-1.5  text-stone-400 text-sm">
+                            <input type="checkbox" className="h-3.5 w-3.5 accent-lime-500" />
+                                remember me?
+                        </label>
+
+                        <button type="button" className="text-sm leading-none text-lime-500 hover:underline">
+                            Forgot password?
+                        </button>
                     </div>
 
                     <button className="bg-lime-500 px-8 py-2 w-max rounded-sm text-white font-semibold hover:bg-lime-600 transition-colors">
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,34 +1,31 @@
+import { User, Lock, Mail } from "lucide-react";
+import { AuthInput } from "../components/AuthInput";
+import { useState } from "react";
 
 export function Register() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log({name, email, password});
+    }
+
     return (
         <div className="flex items-center justify-center bg-stone-100 px-4 min-h-screen ">
             <div className="flex flex-col min-w-sm py-8 px-4 items-center justify-center bg-white rounded-sm shadow-md">
                 <div className="flex flex-col gap-2 items-center justify-center mb-10">
-                    <img src="/images/user.png" alt="user icon" className="h-8 w-8"/>
+                    <User className="h-6 w-6 text-lime-500" />
                     <span className="text-stone-600 text-xl font-bold ">
                         Create account!
                     </span>
                 </div>
 
-                <form className="flex flex-col gap-2 w-full px-4">
-                    <div className="relative mb-6">
-                        <label className="text-semibold mb-1 block text-sm text-stone-400">Name</label>
-                        <input
-                            type="text"
-                            className="w-full border-b border-stone-400 bg-transparent pb-2 pr-8 text-sm text-stone-100 outline-none focus:border-lime-400"
-                        />
-                        {/* <UserIcon className="absolute right-0 bottom-2 h-4 w-4 text-stone-500" /> */}
-                    </div>
-                    
-                    <div className="relative mb-6">
-                        <label className="text-semibold mb-1 block text-sm text-stone-400">Password</label>
-                        <input
-                            type="text"
-                            className="w-full border-b border-stone-400 bg-transparent pb-2 pr-8 text-sm text-stone-100 outline-none focus:border-lime-400"
-                        />
-                        {/* <UserIcon className="absolute right-0 bottom-2 h-4 w-4 text-stone-500" /> */}
-                    </div>
-
+                <form className="flex flex-col gap-2 w-full px-4" onSubmit={handleSubmit}>
+                    <AuthInput label="Name" type="text"  icon={User} value={name} onChange={setName} />
+                    <AuthInput label="E-mail" type="email" icon={Mail} value={email} onChange={setEmail} />
+                    <AuthInput label="Password" type="password" icon={Lock} value={password} onChange={setPassword} />
                     <button className="bg-lime-500 px-8 py-2 w-max rounded-sm text-white font-semibold hover:bg-lime-600 transition-colors">
                         Create
                     </button>
