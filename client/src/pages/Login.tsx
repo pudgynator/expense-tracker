2e21c080e561cbbe101e502681e1a0e582ee6433
Maria <dobrydnyk.m@gmail.com>
2026-09-14 14:15:06 +0300

    change styles

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -15,7 +15,7 @@ export function Login() {
 
     return (
         <div className="flex items-center justify-center bg-stone-100 px-4 min-h-screen ">
-            <div className="flex flex-col min-w-sm py-8 px-4 items-center justify-center bg-white rounded-sm shadow-md">
+            <div className="flex flex-col min-w-sm py-8 px-4 items-center justify-center bg-white rounded-3xl shadow-md">
                 <div className="flex flex-col gap-2 items-center justify-center mb-10">
                     <LogIn className="h-6 w-6 text-violet-500" />
                     <p className="text-stone-600 text-xl font-bold ">
@@ -41,7 +41,7 @@ export function Login() {
                         </button>
                     </div>
 
-                    <button className="bg-violet-500 px-8 py-2 w-max rounded-lg text-white font-semibold hover:bg-violet-600 transition-colors">
+                    <button className="bg-violet-500 px-8 py-2 w-max rounded-3xl text-white font-semibold hover:bg-violet-600 transition-colors">
                         Login
                     </button>
                 </form>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -14,7 +14,7 @@ export function Register() {
 
     return (
         <div className="flex items-center justify-center bg-stone-100 px-4 min-h-screen ">
-            <div className="flex flex-col min-w-sm py-8 px-4 items-center justify-center bg-white rounded-lg shadow-md">
+            <div className="flex flex-col min-w-sm py-8 px-4 items-center justify-center bg-white rounded-3xl shadow-md">
                 <div className="flex flex-col gap-2 items-center justify-center mb-10">
                     <User className="h-6 w-6 text-violet-500" />
                     <span className="text-stone-600 text-xl font-bold ">
@@ -26,7 +26,7 @@ export function Register() {
                     <AuthInput label="Name" type="text"  icon={User} value={name} onChange={setName} />
                     <AuthInput label="E-mail" type="email" icon={Mail} value={email} onChange={setEmail} />
                     <AuthInput label="Password" type="password" icon={Lock} value={password} onChange={setPassword} />
-                    <button className="bg-violet-500 px-8 py-2 w-max rounded-lg text-white font-semibold hover:bg-violet-600 transition-colors">
+                    <button className="bg-violet-500 px-8 py-2 w-max rounded-3xl text-white font-semibold hover:bg-violet-600 transition-colors">
                         Create
                     </button>
                 </form>
